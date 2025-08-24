7e56d3c4d8f2b6ca7df4fd96118ab55e21d0ef4e
Adnan <adnanmuhammedadnancp@gmail.com>
2025-08-24 16:09:04 +0530

    add hero section

---
+++ ui/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+import { Textarea } from "@/components/ui/textarea";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Textarea placeholder="Enter something" className="bg-white/10 backdrop-blur-lg"></Textarea>
+    </div>
+  );
+}
